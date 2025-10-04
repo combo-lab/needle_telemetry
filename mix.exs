52b093304fff2a9dec6a6560f4590fe899331916
Zeke Dou <zeke@zekedou.live>
2025-10-04 23:08:03 +0800

    chore: polish mix alias - tag

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule NeedleTelemetry.MixProject do
 
   defp tag_release(_) do
     Mix.shell().info("Tagging release as v#{@version}")
-    System.cmd("git", ["tag", "v#{@version}"])
+    System.cmd("git", ["tag", "v#{@version}", "--message", "Release v#{@version}"])
     System.cmd("git", ["push", "--tags"])
   end
 end
